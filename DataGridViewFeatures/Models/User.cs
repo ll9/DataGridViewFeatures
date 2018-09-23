93e27b3ec02e2ddff775f377ee71a161e806eb80
ll9 <llwierer@web.de>
2018-09-23 20:54:24 +0200

    Created User

---
+++ DataGridViewFeatures/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridViewFeatures.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public double Height { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+}
